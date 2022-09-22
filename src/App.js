76a2557093a42af5d17cfea5bd46050b8d2dd847
NicolasB <nicolas.becerra@etu.hesge.ch>
2022-09-22 19:19:58 +0200

    frist commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import InputTodo from './components/InputTodo';
+function App() {
+  return (
+    <div className="App">
+      <InputTodo/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/InputTodo.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+
+//export default
+function InputTodo(){
+    const [todo, setTodo] = useState({description: '', date: ''});
+    const [todoList, setTodos] = useState([]);
+    const addTodo = () => {
+        setTodos([...todoList, todo]);//si on veut faire que le dernier soit le premier on met [description, ...todos]on inverse
+        setTodo({description: '', date: ''});
+    }
+    return(
+        <div>
+            <div className='Title'>
+                <h1>Simple Todolist</h1>
+            </div>
+            <br></br>
+            <fieldset>
+                <legend align="left"> Add todo : </legend>
+                <label> Description : </label>
+                <input 
+                    type="text" 
+                    value={todo.description}
+                    onChange={event=>setTodo({...todo, description: event.target.value})}>    
+                </input>
+                <label> Date : </label>
+                <input 
+                    type="date"
+                    value={todo.date}
+                    onChange={event=>setTodo({...todo, date: event.target.value})}>
+                </input>
+                <button 
+                    onClick={addTodo}>
+                    Add
+                </button>
+            </fieldset>
+            <table className='Text'>
+                <tr>
+                    <th>Date</th><th>Description</th>
+                </tr>
+                <tbody>
+                    {todoList.map((todos, index) => 
+                        <tr className='TableTr' 
+                            key={index}>
+                            <td>{todos.date}</td>
+                            <td>{todos.description}</td>
+                        </tr>)}
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default InputTodo // ici ou direct dans le titre de la fuction du componnet

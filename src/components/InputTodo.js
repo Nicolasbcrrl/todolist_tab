a350c2764ef76190512e37ed3a04d183367e5bf0
NicolasB <nicolas.becerra@etu.hesge.ch>
2022-10-04 00:16:32 +0200

    first commit

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,18 +1,36 @@
-import React, { useState } from 'react';
-import Table from './Table';
+import React, { useState,useRef } from 'react';
+import { AgGridReact } from 'ag-grid-react'; // the AG Grid React Component
+
+import 'ag-grid-community/styles/ag-grid.css'; // Core grid CSS, always needed
+import 'ag-grid-community/styles/ag-theme-material.css'; // Optional theme CSS
 
 //export default
 function InputTodo(){
-    const [todo, setTodo] = useState({description: '', date: ''});
+    const [todo, setTodo] = useState({description: '', date: '', priority: ''});
     const [todoList, setTodos] = useState([]);
     const addTodo = () => {
         setTodos([...todoList, todo]);//si on veut faire que le dernier soit le premier on met [description, ...todos]on inverse
         setTodo({description: '', date: ''});
     }
-    const deleteTodo = (row)=>{
-        console.log("Button pressed");
-        setTodos(todoList.filter((todo, index)=> index !== row)); //ici on va faire une nouvelle list qui va exclure l'element à l'index et retourner une nouvelle liste avec les elements qui ne sont pas à cette index
+    const deleteTodo = ()=>{
+        if(gridRef.current.getSelectedNodes().length >0){
+            console.log(gridRef.current.getSelectedNodes()[0].childIndex);
+            //setTodos(todoList.filter((todo, index)=> index !== row)); //ici on va faire une nouvelle list qui va exclure l'element à l'index et retourner une nouvelle liste avec les elements qui ne sont pas à cette index
+            setTodos(todoList.filter((todo, index)=> index !== gridRef.current.getSelectedNodes()[0].childIndex));
+        }
+        else{
+            alert('Please select a row to delete');
+        }
     };
+    const [columnDefs] = useState([
+        {field: 'description', sortable: true, filter: true, floatingFilter: true, animateRows: true},
+        {field: 'date', sortable: true, filter: true, floatingFilter: true, animateRows: true},
+        {field: 'priority', sortable: true, filter: true, 
+            cellStyle: params => params.value === '1' ? {color: 'red'} : params.value === '3' ? {color: 'green'} : {color: 'orange'}, 
+            floatingFilter: true, animateRows: true
+        },
+    ])
+    const gridRef = useRef();
 
     return(
         <div>
@@ -34,12 +52,27 @@ function InputTodo(){
                     value={todo.date}
                     onChange={event=>setTodo({...todo, date: event.target.value})}>
                 </input>
+                <label> Priority : </label>
+                <input
+                    type="number"
+                    value={todo.priority}
+                    onChange={event=>setTodo({...todo, priority: event.target.value})}>
+                </input>
                 <button 
                     onClick={addTodo}>
                     Add
                 </button>
+                <button onClick={deleteTodo} >Delete</button>
             </fieldset>
-            <Table todoList={todoList} deleteTodo={deleteTodo}/>
+            <div className='ag-theme-material' style={{width: '50%', height: 500, margin:'auto'}}>
+                <AgGridReact
+                    ref={gridRef}
+                    onGridReady={params => gridRef.current = params.api}
+                    rowSelection="single"
+                    rowData={todoList}
+                    columnDefs={columnDefs}
+            />
+        </div>
         </div>
     );
 }
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,22 +1,26 @@
-import React from "react";
+import React, { useState, useRef} from "react";
+import { AgGridReact } from 'ag-grid-react'; // the AG Grid React Component
+
+import 'ag-grid-community/styles/ag-grid.css'; // Core grid CSS, always needed
+import 'ag-grid-community/styles/ag-theme-material.css'; // Optional theme CSS
+
  
 function Table(props){
+    const [columnDefs] = useState([
+        {field: 'description', sortable: true, filter: true},
+        {field: 'date', sortable: true, filter: true},
+        {field: 'priority', sortable: true, filter: true, cellStyle: params => params.value === '1' ? {color: 'red'} : params.value === '3' ? {color: 'green'} : {color: 'orange'}},
+    ])
+    const gridRef = useRef();
     return(
-        <div>
-            <table className='Text'>
-                <tr>
-                    <th>Date</th><th>Description</th>
-                </tr>
-                <tbody>
-                    {props.todoList.map((todos, index) => 
-                        <tr className='TableTr' 
-                            key={index}>
-                            <td>{todos.date}</td>
-                            <td>{todos.description}</td>
-                            <td><button onClick={()=>props.deleteTodo(index)}> Delete </button></td>
-                        </tr>)}
-                </tbody>
-            </table>
+        <div className='ag-theme-material' style={{width: '35%', height: 500, margin:'auto'}}>
+            <AgGridReact
+                ref={gridRef}
+                onGridReady={params => gridRef.current = params.api}
+                rowSelection="single"
+                rowData={props.todoList}
+                columnDefs={columnDefs}
+            />
         </div>
     );
 }

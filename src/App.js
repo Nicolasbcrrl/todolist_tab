f552c627623e14e63c38606ab58a54c8e410dc53
NicolasB <nicolas.becerra@etu.hesge.ch>
2022-10-23 23:22:01 +0200

    add tab

--- src/App.js
+++ src/App.js
@@ -3,19 +3,47 @@ import InputTodo from './components/InputTodo';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import { Box, Tab, Tabs } from '@mui/material';
+import { useState } from 'react';
+
 function App() {
+  const [tabIndex, setTabIndex] = useState(0);
+
+  const handleTabChange = (event, newTabIndex) => {
+    setTabIndex(newTabIndex);
+  };
+
   return (
     <div className="App">
-      <AppBar
-        position="static"
-      > 
-        <Toolbar>
-          <Typography variant="h6">
-            My Todo List
-          </Typography> 
-        </Toolbar>
-      </AppBar>
-      <InputTodo/>
+      <Box>
+          <Box>
+            <Tabs value={tabIndex} onChange={handleTabChange}>
+              <Tab label = "Home"/>
+              <Tab label = "Todos"/>
+            </Tabs>
+          </Box>
+          <Box sx={{ padding: 2 }}>
+            {tabIndex === 0 && (
+              <Box>
+                <Typography>Welcome to my TodoList</Typography>
+              </Box>
+            )}
+            {tabIndex === 1 && (
+              <Box>
+                <AppBar
+                  position="static"
+                > 
+                  <Toolbar>
+                    <Typography variant="h6">
+                     My Todo List
+                    </Typography> 
+                  </Toolbar>
+                </AppBar>
+                <InputTodo/>
+              </Box>
+            )}
+          </Box>
+      </Box>
     </div>
   );
 }
